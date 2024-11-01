6738b1a756bfedddd58ef4a5d3466517bb90f4ca
missxiaolin <462441355@qq.com>
2024-11-01 14:49:24 +0800

    * ceshi

--- index.js
+++ index.js
@@ -2,23 +2,61 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const hostname = "127.0.0.1";
-const port = 3000;
-
 const server = http.createServer((req, res) => {
-  fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
-    if (err) {
-      res.statusCode = 500;
-      res.setHeader("Content-Type", "text/plain");
-      res.end("Error loading index.html");
+  // 获取请求的URL
+  const url = req.url;
+  console.log(url)
+  // 根据URL进行路由处理
+  if (url === "/" || url.includes('.html')) {
+    let indexPath
+    // 返回index.html页面
+    if (url.includes('.html')) {
+      indexPath = path.join(__dirname, url);
     } else {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      res.end(data);
+      indexPath = path.join(__dirname, "index.html");
     }
-  });
+    
+    fs.readFile(indexPath, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  } else if (url.indexOf(".js")) {
+    // 返回script.js文件
+    const scriptPath = path.join(__dirname, "script.js");
+    fs.readFile(scriptPath, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+      } else {
+        res.writeHead(200, { "Content-Type": "application/javascript" });
+        res.end(data);
+      }
+    });
+  } else if (url.includes(".css")) {
+    // 返回style.css文件
+    const stylePath = path.join(__dirname, "style.css");
+    fs.readFile(stylePath, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/css" });
+        res.end(data);
+      }
+    });
+  } else {
+    // 其他路由处理
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+  }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// 监听端口
+server.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
 });
